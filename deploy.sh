20b19ba7afeda057abe767f9ad45b5a17bb997d2
lhoangan <hoang-an.le@uva.nl>
2017-07-15 21:00:17 +0200

    move install script for zsh to different file

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,6 @@ install_dir=$HOME/bin
 mkdir -p "${install_dir}"
 # =============================================================================
 # INSTALL ZSH
-#wget ftp://ftp.zsh.org/pub/zsh-5.3.tar.gz
-tar xvf zsh-5.3.tar.gz
-mv zsh-5.3 "${install_dir}"/zsh
-cat .profile >> $HOME/.profile
 
 # ============================================================================
 # SHELL CONFIGS
