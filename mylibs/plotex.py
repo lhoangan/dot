6f3a404ed5bad6b347e2906e1290a62848ae4d64
Hoang-An Le <hoang-an.le@uva.nl>
2018-05-11 15:31:35 +0200

    Update

--- mylibs/plotex.py
+++ mylibs/plotex.py
@@ -1,8 +1,13 @@
 import os
-os.environ['FIGSIZE'] = '496.7855'
-print 'Default Figure size is set to 496.7855 of CVPR'
+if 'FIGSIZE' not in os.environ:
+    os.environ['FIGSIZE'] = '496.7855'
+print 'Default Figure size is set to ', os.environ['FIGSIZE']
 print 'Check your number by running \\the\\linewidth or \\the\\textwidth in LaTeX'
-print 'Apply the new number by overwrite it to os.environ[\'FIGSIZE\']'
+print 'Apply the new number by overwrite it to os.environ[\'FIGSIZE\'] and reload'
+print 'the module with reload(plotex)'
+print 'Some useful textwidth numbers can be found below'
+print '\\the\\textwidth for CVPR : 496.85625 \t for ECCV : 347.12361'
+print '\\the\\linewidth for CVPR : 237.135594'
 import numpy as np
 import matplotlib as mpl
 mpl.use('pgf')
@@ -24,11 +29,11 @@ pgf_with_latex = {                      # setup matplotlib to use latex for outp
     "font.serif": [],                   # blank entries should cause plots to inherit fonts from the document
     "font.sans-serif": [],
     "font.monospace": [],
-    "font.size": 14,
-    "axes.labelsize": 14,               # LaTeX default is 10pt font.
-    "legend.fontsize": 12,              # Make the legend/label fonts a little smaller
-    "xtick.labelsize": 12,
-    "ytick.labelsize": 12,
+    "font.size": 12,
+    "axes.labelsize": 12,               # LaTeX default is 10pt font.
+    "legend.fontsize": 10,              # Make the legend/label fonts a little smaller
+    "xtick.labelsize": 10,
+    "ytick.labelsize": 10,
     "figure.figsize": figsize(0.95),     # default fig size of 0.9 textwidth
     "pgf.preamble": [
         r"\usepackage[utf8x]{inputenc}",    # use utf8 fonts becasue your computer can handle it :)
