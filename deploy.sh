f8325dd2da96eaeb5165eaed855e05018d36eac9
Hoang-An LE <lhoangan@gmail.com>
2018-11-21 21:51:12 +0100

    Add myprompt to bashrc

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ mkdir -p "${install_dir}"
     mv -v ${HOME}/.my_config ${HOME}/${bk} # rename it with a datetime id
 }
 echo 'Creating new .myconfig'
-ln -sf .myconfig ${HOME}/
+ln -sf ${PWD}/.myconfig ${HOME}/
 
 #-------------------------------------------------------------------------------  
 # Add supplementary config to shell config
@@ -62,44 +62,45 @@ ln -sf ${PWD}/.dircolors ${HOME}/
 }
 echo 'Creating new .myprompt'
 ln -sf ${PWD}/.myprompt ${HOME}/
+echo source ${HOME}/.myprompt >> ${HOME}/.bashrc
 
-# =============================================================================
-# INSTALL VIM
-
-# check if anaconda is available in /home/anaconda2
-if [ ! -d ${anaconda} ]; then
-    echo "CANNOT FIND "${anaconda}". Install ANACONDA into "$(dirname ${HOME})" and try again"
-    exit
-fi
-
-
-# TODO: check if vim is available, install if needed or build from scratch
-echo 'INSTALL VIM.....'
-./install_vim.sh "${install_dir}" ${anaconda}/bin/python-config ${anaconda}/lib
-
-# 1. Download vim-plug, a plugin manager, into ${HOME}/.vim/autoload
-# check if a folder named .vim is already existed in the home dir
-[ -w ${HOME}/.vim ] && {
-    bk=.vim_bk_"$(date +"%y%m%d_%H%M%S")"
-    echo 'Found old .vim directory. Backing up to '${bk}
-    mv -v ${HOME}/.vim ${HOME}/${bk} # rename it with a datetime id
-}
-# detailed can be found here: https://github.com/junegunn/vim-plug
-curl -fLo ${HOME}/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# 2. copy .vimrc from dot folder to home folder
-# check if .vimrc is already existed in the home dir
-[ -w ${HOME}/.vimrc ] && {
-    dt=$(date +"%y%m%d_%H%M%S")
-    echo 'Found old .vimrc. Backing up...'
-    mv -v ${HOME}/.vimrc ${HOME}/.vimrc_bk_"${dt}" # rename it with a datetime id
-}
-
-# copy vim config to home folders
-echo 'Creating new .vim and .vimrc to '${HOME}
-ln -s .vimrc ${HOME}/
-cp -r .vim/colors ${HOME}/.vim
-cp PaperColor.vim ${HOME}/.vim/colors/
+## =============================================================================
+## INSTALL VIM
+#
+## check if anaconda is available in /home/anaconda2
+#if [ ! -d ${anaconda} ]; then
+#    echo "CANNOT FIND "${anaconda}". Install ANACONDA into "$(dirname ${HOME})" and try again"
+#    exit
+#fi
+#
+#
+## TODO: check if vim is available, install if needed or build from scratch
+#echo 'INSTALL VIM.....'
+#./install_vim.sh "${install_dir}" ${anaconda}/bin/python-config ${anaconda}/lib
+#
+## 1. Download vim-plug, a plugin manager, into ${HOME}/.vim/autoload
+## check if a folder named .vim is already existed in the home dir
+#[ -w ${HOME}/.vim ] && {
+#    bk=.vim_bk_"$(date +"%y%m%d_%H%M%S")"
+#    echo 'Found old .vim directory. Backing up to '${bk}
+#    mv -v ${HOME}/.vim ${HOME}/${bk} # rename it with a datetime id
+#}
+## detailed can be found here: https://github.com/junegunn/vim-plug
+#curl -fLo ${HOME}/.vim/autoload/plug.vim --create-dirs \
+#    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+## 2. copy .vimrc from dot folder to home folder
+## check if .vimrc is already existed in the home dir
+#[ -w ${HOME}/.vimrc ] && {
+#    dt=$(date +"%y%m%d_%H%M%S")
+#    echo 'Found old .vimrc. Backing up...'
+#    mv -v ${HOME}/.vimrc ${HOME}/.vimrc_bk_"${dt}" # rename it with a datetime id
+#}
+#
+## copy vim config to home folders
+#echo 'Creating new .vim and .vimrc to '${HOME}
+#ln -s .vimrc ${HOME}/
+#cp -r .vim/colors ${HOME}/.vim
+#cp PaperColor.vim ${HOME}/.vim/colors/
 
 # install vim plugins
 vim +PlugInstall +qall
@@ -115,9 +116,9 @@ vim +PlugInstall +qall
 }
 # copy my config
 echo 'Creating new .tmux config to '${HOME}
-ln -s .tmux.conf ${HOME}/
-ln -s .tmux.ver ${HOME}/
-ln -s .tmux.hor ${HOME}/
+ln -sf ${PWD}/.tmux.conf ${HOME}/
+ln -sf ${PWD}/.tmux.ver ${HOME}/
+ln -sf ${PWD}/.tmux.hor ${HOME}/
 
 
 #=============================================================================
