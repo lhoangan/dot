5548e30b91e7d1a43e2c76881a6592fa1587c9ab
Hoang-An LE <lhoangan@gmail.com>
2018-11-21 21:01:48 +0100

    Link config files rather than copying

--- deploy.sh
+++ deploy.sh
@@ -3,89 +3,114 @@
 # useful utility:
 # sudo apt-get install git diffcolor, tree
 
-install_dir=$HOME/bin
+install_dir=${HOME}/bin
+anaconda=${HOME}/anaconda2
+
+mkdir -p "${install_dir}"
+# =============================================================================
+# INSTALL BASH_IT
+#./install_bashit.sh
 
 # ============================================================================
 # SHELL CONFIGS
-cp .my_config $HOME/
-source $HOME/.my_config
-# Add suplementary config to shell config
-[ -w ~/.zshrc ] && {
-    #cat .my_config >> $HOME/.zshrc
-    echo source ${HOME}/.my_config >> $HOME/.zshrc
-} || {
-    #cat .my_config >> $HOME/.bashrc
-    echo source ${HOME}/.my_config >> $HOME/.bashrc
+[ -w ~/.my_config ] && {
+    bk=.my_config_bk_"$(date +"%y%m%d_%H%M%S")"
+    echo 'Found old .my_config file. Backing up to '${bk}
+    mv -v ${HOME}/.my_config ${HOME}/${bk} # rename it with a datetime id
+}
+echo 'Creating new .my_config'
+ln -sf .my_config ${HOME}/
+
+#-------------------------------------------------------------------------------  
+# Add supplementary config to shell config
+[ -w ~/.bashrc ] && {
+    bk=.bashrc_bk_"$(date +"%y%m%d_%H%M%S")"
+    echo 'Found old .bashrc file. Backing up to '${bk}
+    mv -v ${HOME}/.bashrc ${HOME}/${bk} # rename it with a datetime id
 }
 
-# =============================================================================
-# BASH CONFIGURATION
-[ -w $HOME/.bashrc ] && {
-    dt=$(date +"%y%m%d_%H%M%S")
-    mv $HOME/.bashrc $HOME/.bashrc.bk_"${dt}" # rename it with a datetime id
+echo 'Creating new .bashrc'
+cp -v .bashrc ${HOME}
+echo source ${HOME}/.my_config >> ${HOME}/.bashrc
+
+#-------------------------------------------------------------------------------  
+# link .inputrc to ${HOME}
+[ -w ~/.inputrc ] && {
+    bk=.inputrc_bk_"$(date +"%y%m%d_%H%M%S")"
+    echo 'Found old .inputrc file. Backing up to '${bk}
+    mv -v ${HOME}/.inputrc ${HOME}/${bk} # rename it with a datetime id
 }
+echo 'Creating new .inputrc'
+ln -sf ${PWD}/.inputrc ${HOME}/
 
-# uncomment to instal bash_it
-#./install_bashit.sh
-
-# install ls color
-wget https://raw.github.com/trapd00r/LS_COLORS/master/LS_COLORS -O $HOME/.dircolors
-echo 'eval $(dircolors -b $HOME/.dircolors)' >> $HOME/.my_config
-
+#-------------------------------------------------------------------------------  
+# link .my_prompt to ${HOME}
+[ -w ~/.my_prompt ] && {
+    bk=.my_prompt_bk_"$(date +"%y%m%d_%H%M%S")"
+    echo 'Found old .my_prompt file. Backing up to '${bk}
+    mv -v ${HOME}/.my_prompt ${HOME}/${bk} # rename it with a datetime id
+}
+echo 'Creating new .my_prompt'
+ln -sf ${PWD}/.my_prompt ${HOME}/
 
 # =============================================================================
 # INSTALL VIM
 
-mkdir -p "${install_dir}"
-
 # check if anaconda is available in /home/anaconda2
-if [ ! -d $HOME/anaconda2 ]; then
-    echo "CANNOT FIND "$HOME/anaconda2". Install ANACONDA into "$HOME" and try again"
+if [ ! -d ${anaconda} ]; then
+    echo "CANNOT FIND "${anaconda}". Install ANACONDA into "$(dirname ${HOME})" and try again"
     exit
 fi
 
 
 # TODO: check if vim is available, install if needed or build from scratch
 echo 'INSTALL VIM.....'
-./install_vim.sh "${install_dir}" $HOME/anaconda2/bin/python-config $HOME/anaconda2/lib
+./install_vim.sh "${install_dir}" ${anaconda}/bin/python-config ${anaconda}/lib
 
-# 1. Download vim-plug, a plugin manager, into $HOME/.vim/autoload
+# 1. Download vim-plug, a plugin manager, into ${HOME}/.vim/autoload
 # check if a folder named .vim is already existed in the home dir
-[ -w $HOME/.vim ] && {
-    dt=$(date +"%y%m%d_%H%M%S")
-    mv $HOME/.vim $HOME/.vim_bk_"${dt}" # rename it with a datetime id
+[ -w ${HOME}/.vim ] && {
+    bk=.vim_bk_"$(date +"%y%m%d_%H%M%S")"
+    echo 'Found old .vim directory. Backing up to '${bk}
+    mv -v ${HOME}/.vim ${HOME}/${bk} # rename it with a datetime id
 }
 # detailed can be found here: https://github.com/junegunn/vim-plug
-curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
+curl -fLo ${HOME}/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # 2. copy .vimrc from dot folder to home folder
 # check if .vimrc is already existed in the home dir
-[ -w $HOME/.vimrc ] && {
+[ -w ${HOME}/.vimrc ] && {
     dt=$(date +"%y%m%d_%H%M%S")
-    mv $HOME/.vimrc $HOME/.vimrc_bk_"${dt}" # rename it with a datetime id
+    echo 'Found old .vimrc. Backing up...'
+    mv -v ${HOME}/.vimrc ${HOME}/.vimrc_bk_"${dt}" # rename it with a datetime id
 }
 
 # copy vim config to home folders
-cp .vimrc $HOME/
-cp -r .vim/colors $HOME/.vim
+echo 'Creating new .vim and .vimrc to '${HOME}
+ln -s .vimrc ${HOME}/
+cp -r .vim/colors ${HOME}/.vim
 
 # install vim plugins
 vim +PlugInstall +qall
 
 
-
 # ============================================================================
 # TMUX CONFIGS
 # TODO: check if tmux available, install if needed (or build from source?)
-[ -w $HOME/.tmux.conf ] && {
+[ -w ${HOME}/.tmux.conf ] && {
     dt=$(date +"%y%m%d_%H%M%S")
-    mv $HOME/.tmux.conf $HOME/.tmux.conf_bk_"${dt}" # rename it with a datetime id
+    echo 'Found old .tmux.conf. Backing up...'
+    mv -v ${HOME}/.tmux.conf ${HOME}/.tmux.conf_bk_"${dt}" # rename it with a datetime id
 }
 # copy my config
-cp .tmux.conf $HOME/
-cp .tmux.ver $HOME/
-cp .tmux.hor $HOME/
+echo 'Creating new .tmux config to '${HOME}
+ln -s .tmux.conf ${HOME}/
+ln -s .tmux.ver ${HOME}/
+ln -s .tmux.hor ${HOME}/
 
+
+#=============================================================================
+source ${HOME}/.bashrc
 # zsh, build from source?
 # download from http://www.zsh.org/pub/zsh.tar.gz
 # extract it
